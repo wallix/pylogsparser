b3a94f0e2c3e4b77ed26100420bea4d7fc26b5f9
Frédéric Lepied <frederic.lepied@wallix.com>
2011-07-24 09:11:25 +0200

    fix automatic merge for s3 test

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -79,11 +79,14 @@ class TestSample(unittest.TestCase):
     def test_normalize_samples_012_symantec(self):
         self.normalize_samples('symantec.xml', 'symantec', 0.99)
     
+    def test_normalize_samples_013_msexchange2007MTL(self):
+        self.normalize_samples('MSExchange2007MessageTracking.xml', 'MSExchange2007MessageTracking', 0.99)
+
     def test_normalize_samples_014_arkoonfast360(self):
         self.normalize_samples('arkoonFAST360.xml', 'arkoonFAST360', 0.99)
 
-    def test_normalize_samples_013_msexchange2007MTL(self):
-        self.normalize_samples('MSExchange2007MessageTracking.xml', 'MSExchange2007MessageTracking', 0.99)
+    def test_normalize_samples_015_s3(self):
+        self.normalize_samples('s3.xml', 's3', 0.99)
 
 
 class TestCSVPattern(unittest.TestCase):
@@ -251,9 +254,6 @@ log["date"] = newdate
         self.assertEqual(ret['date'], 'Jul 18 08:55:35')
         self.assertEqual(ret['id'], '83')
         self.assertEqual(ret['msg'], 'start listening on =127.0.0.1 pam auth started')
-
-    def test_normalize_samples_012_s3(self):
-        self.normalize_samples('s3.xml', 's3', 0.99)
 
 if __name__ == "__main__":
     unittest.main()
