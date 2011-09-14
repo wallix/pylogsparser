3f1d852e73a7dc46f418b65afc1bf764ce7d4165
Matthieu Huin <mhu@wallix.com>
2011-09-14 12:25:32 +0200

    related to IssueID #2537 fixes unexpectedly shortened body when using the snare normalizer in WLB.

--- tests/test_log_samples.py
+++ tests/test_log_samples.py
@@ -495,7 +495,7 @@ class Test(unittest.TestCase):
                  'source_host': 'a-zA-Z0-9_',
                  'audit_event_category': 'None',
                  'program' : 'EventLog',
-                 'body': unicode('Le service s’est arrêté.	119', 'utf8')})
+                 'body': unicode('Le service s’est arrêté.	119 ', 'utf8')})
 
 	self.aS(unicode("""<13> Aug 31 15:46:47 a-zA-Z0-9_ MSWinEventLog	0	Security	284	ven. août 26 16:42:01 201	4689	Microsoft-Windows-Security-Auditing	A-ZA-Z0-9_\\clo	N/A	Success Audit	a-zA-Z0-9_	Fin du processus	 Un processus est terminé. Sujet : ID de sécurité : S-1-5-21-2423214773-420032381-3839276281-1000 Nom du compte : clo Domaine du compte : A-ZA-Z0-9_ ID d’ouverture de session : 0x21211 Informations sur le processus : ID du processus : 0xb4c Nom du processus : C:\\Windows\\System32\\taskeng.exe État de fin : 0x0	138 """, 'utf8'),
                 {'snare_event_log_type': 'MSWinEventLog',
@@ -510,7 +510,7 @@ class Test(unittest.TestCase):
                  'source_host': 'a-zA-Z0-9_',
                  'audit_event_category': 'Fin du processus',
                  'program' : "EventLog",
-                 'body': unicode('Un processus est terminé. Sujet : ID de sécurité : S-1-5-21-2423214773-420032381-3839276281-1000 Nom du compte : clo Domaine du compte : A-ZA-Z0-9_ ID d’ouverture de session : 0x21211 Informations sur le processus : ID du processus : 0xb4c Nom du processus : C:\\Windows\\System32\\taskeng.exe État de fin : 0x0	138', 'utf8')})
+                 'body': unicode('Un processus est terminé. Sujet : ID de sécurité : S-1-5-21-2423214773-420032381-3839276281-1000 Nom du compte : clo Domaine du compte : A-ZA-Z0-9_ ID d’ouverture de session : 0x21211 Informations sur le processus : ID du processus : 0xb4c Nom du processus : C:\\Windows\\System32\\taskeng.exe État de fin : 0x0	138 ', 'utf8')})
 
 #	self.aS(unicode("""clo-vbox-win-7 MSWinEventLog	0	Security	201	mer. août 31 15:46:45 201	4689	Microsoft-Windows-Security-Auditing	CLO-VBOX-WIN-7\clo	N/A	Success Audit	clo-vbox-win-7	Fin du processus		Un processus est terminé.    Sujet :   ID de sécurité :  S-1-5-21-2423214773-420032381-3839276281-1000   Nom du compte :  clo   Domaine du compte :  CLO-VBOX-WIN-7   ID d’ouverture de session :  0x260060    Informations sur le processus :   ID du processus : 0xb10   Nom du processus : C:\Windows\System32\SearchProtocolHost.exe   État de fin : 0x0	158 """, 'utf8'),
 #{}
