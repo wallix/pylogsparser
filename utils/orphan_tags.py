7cff8275d4781e2432415242280d800605af48d3
Matthieu Huin <mhu@wallix.com>
2012-01-19 18:10:17 +0100

    IssueID #2997 - refines data handling and help message

--- utils/orphan_tags.py
+++ utils/orphan_tags.py
@@ -22,8 +22,11 @@
 
 """Utility to visualize tags per taxonomy."""
 
+
+
 import time
 import os
+from sys import exit as sysexit
 from logsparser.lognormalizer import LogNormalizer as LN
 from optparse import OptionParser
 
@@ -31,7 +34,33 @@ normalizer_path = os.environ['NORMALIZERS_PATH'] or '../normalizers/'
 
 ln = LN(normalizer_path)
 
-parser = OptionParser()
+help = """
+This utility aims to list tags frequencies per taxonomy.
+
+It uses the environment variable $NORMALIZERS_PATH as the source of normalizers
+to test.
+By default, this script will use the sample logs shipped in the normalizers as
+its input; it is possible to use another log file by using the parameter -i.
+
+The script's output is a classification of tags per service type. It looks like
+this:
+
+Category *SERVICENAME* (N log(s)):
+
+	* tag1 : 16.67%
+		( program : 100.00%  )
+	* tag2 : 16.67%
+		( program : 100.00%  )
+	...
+	
+N is the total amount of logs in the input set (sample logs included) that
+match this category.
+For each tag, the occurrence percentage is displayed; on the following line
+the repartition of the logs with this tag, in this category, per program, is
+displayed.
+"""
+
+parser = OptionParser(help)
 parser.add_option("-i", 
                   "--input", 
                   dest="input", 
@@ -40,6 +69,7 @@ parser.add_option("-i",
 (options, args) = parser.parse_args()
 
 logs = []
+
 if options.input:
     try:
         logs = open(options.input, 'r').readlines()
@@ -76,10 +106,16 @@ def compute(logs_set):
         logs_per_categories[taxonomy]['logs'] += 1
         for t in testlog:
             if t not in logs_per_categories[taxonomy]['tags']:
-                logs_per_categories[taxonomy]['tags'][t] = 0
-            logs_per_categories[taxonomy]['tags'][t] += 1
+                logs_per_categories[taxonomy]['tags'][t] = {'_total' : 0}
+            prg = "Not set"
+            if "program" in testlog:
+                prg = testlog['program']
+            if prg not in logs_per_categories[taxonomy]['tags'][t]:
+                logs_per_categories[taxonomy]['tags'][t][prg] = 0
+            logs_per_categories[taxonomy]['tags'][t]['_total'] += 1
+            logs_per_categories[taxonomy]['tags'][t][prg] += 1
 
-print "Parsing logs...",
+print "Parsing %i logs..." % (len(base_logs) + len(logs)),
 start = time.time()
 compute(base_logs)
 compute(logs)
@@ -91,10 +127,14 @@ for c in categories:
         total = float(logs_per_categories[c]['logs'])
         print "Category %s (%i log(s)):\n" % (c, total)
         for t in sorted(logs_per_categories[c]['tags'].keys(), 
-                        cmp = lambda x,y: cmp(logs_per_categories[c]['tags'][x],
-                                              logs_per_categories[c]['tags'][y])
+                        cmp = lambda x,y: cmp(logs_per_categories[c]['tags'][x]['_total'],
+                                              logs_per_categories[c]['tags'][y]['_total'])
                        ):
-            print "\t* %s : %.2f%%" % (t, 100 * logs_per_categories[c]['tags'][t] / total)
+            print "\t* %s : %.2f%%" % (t, 100 * logs_per_categories[c]['tags'][t]['_total'] / total)
+            print "\t\t(",
+            for prg in [ u for u in logs_per_categories[c]['tags'][t] if u is not '_total' ]:
+                print "%s : %.2f%% " % (prg, 100 * float(logs_per_categories[c]['tags'][t][prg]) / logs_per_categories[c]['tags'][t]['_total']),
+            print ")"
     else:
         print "No logs for category %s !" % c
     print "\n-------------------\n"
