cbd2bce12cdb779ad91bbc662cb6c64d5c051806
mhu <mhu@wallix.com>
2012-11-14 18:34:53 +0100

    IssueID #4008 - Fixes stupid typo in test, fixes patterns for better handling of carriage returns, adds examples whenever possible

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -148,10 +148,10 @@ class TestSample(unittest.TestCase):
         
     def test_normalize_samples_032_squidguard(self):
         self.normalize_samples('squidguard.xml', 'squidguard', 0.99)
-
-	def test_normalize_samples_033_eventlog_security_audit_w2003en(self):
-		self.normalize_samples('eventlog_security_audit_windows2003_en.xml', 'EventLog-Security-Windows2003[EN]', 0.99)
-		self.normalize_samples('eventlog_security_audit_windows2003_en_2.xml', 'EventLog-Security-Windows2003[EN]_2', 0.99)
+        
+    def test_normalize_samples_033_eventlogW2003(self):
+        self.normalize_samples('eventlog_security_audit_windows2003_en.xml', 'EventLog-Security-Windows2003[EN]_1', 0.99)
+        self.normalize_samples('eventlog_security_audit_windows2003_en_2.xml', 'EventLog-Security-Windows2003[EN]_2', 0.99)
 
 class TestCSVPattern(unittest.TestCase):
     """Test CSVPattern behaviour"""
